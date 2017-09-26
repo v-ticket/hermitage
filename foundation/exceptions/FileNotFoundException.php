91a573fbf453d1809adb95d83b63fc9f6610ae71
g.dresviannykov <g.dresviannykov@concert.ua>
2017-09-26 13:07:21 +0300

    file-storage

---
+++ foundation/exceptions/FileNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: g.dresviannykov
+ * Date: 26.09.2017
+ * Time: 12:48
+ */
+
+namespace livetyping\hermitage\foundation\exceptions;
+
+class FileNotFoundException
+{
+
+}
